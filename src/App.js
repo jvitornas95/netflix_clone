17d43f0fdc36f1a88ffe5a871313772a7fab954d
João Vitor <jvitornas95@gmail.com>
2021-09-23 22:23:52 -0300

    'OK'

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import FeatureMovie from './components/FeatureMovie';
+import Header from './components/Header';
 
 // eslint-disable-next-line
 export default () => {
 
   const [movieList, setMovieList] = useState([]); //usando state
   const [featureData, setFeatureData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(null)
 
   useEffect(() => { // carrega apos a pagina ser recarregada;
 
@@ -31,8 +33,27 @@ export default () => {
     loadAll()
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, [])
+
   return (
     <div className="page">
+
+      <Header black={blackHeader} />
 
       {featureData &&
         <FeatureMovie item={featureData} />
@@ -43,6 +64,12 @@ export default () => {
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+      
+      <footer>
+        Aula B7Web <br/>
+        Direitor de imagem Netflix <br/>
+        Dados usados do site themoviedb.org <br/>
+      </footer>
     </div>
   );
 }
--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -3,7 +3,12 @@ import './FeatureMovie.css'
 
 export default ({ item }) => {
 
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
     
+    for (let i in item.genres){
+        genres.push(item.genres[0].name)
+    }
 
     return (
         <section className="featured" style={{
@@ -17,11 +22,14 @@ export default ({ item }) => {
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="featured--year">2020</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons != 1 ? 's' : ''}</div>
                         <div className="featured--description">{item.overview}</div>
-
-                        <div className="featured--genres"> <strong>Generos: </strong> ...</div>
+                        <div className="featured--button">
+                            <a className="featured--whatchbutton" href="">▶ Assistir</a>
+                            <a className="featured--mylistbutton" href="">Minha lista</a>
+                        </div>
+                        <div className="featured--genres"> <strong>Generos: {genres.join(', ')}</strong> </div>
                     </div>
 
                 </div>
